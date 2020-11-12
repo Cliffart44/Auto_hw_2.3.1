35a004071b5f83923c09f9821c88278af46c4710
Cliffart <Cliffart44@gmail.com>
2020-11-12 12:19:45 +0300

    Otherwise manner

--- src/test/java/ru/netology/test/PageUiTest.java
+++ src/test/java/ru/netology/test/PageUiTest.java
@@ -12,25 +12,18 @@ public class PageUiTest {
     @Test
     void shouldResubmitRequest() {
         open("http://localhost:9999/");
-        $("[data-test-id=city] .input__control").setValue(getCity());
-        if ($$(".input__menu .menu-item__control").size() == 0)
-            $("[data-test-id=city] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, getAnotherCity());
-        if ($$(".input__menu .menu-item__control").size() == 0)
-            $("[data-test-id=city] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, getLattermostCity());
-        if ($$(".input__menu .menu-item__control").size() == 0)
-            $("[data-test-id=city] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, "ро");
-        $$(".input__menu .menu-item__control").first().click();
-        $("[data-test-id=date] [placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, when(false));
+        $("[data-test-id=city] .input__control").setValue(getCityInternally());
+        $("[data-test-id=date] [placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, when(0));
         $("[data-test-id=name] [name=name]").setValue(getFullName());
         $("[data-test-id=phone] [name=phone]").setValue(getPhone());
         $("[data-test-id=agreement]>.checkbox__box").click();
 //        sleep(2000); // For the good of test's sustainability in head mode
         $(".grid-col .button__text").click();
 //        sleep(1000); // For the good of test's sustainability in head mode
-        $("[data-test-id='success-notification']>.notification__content").shouldHave(text("Встреча успешно запланирована на " + when(false))).shouldBe(visible);
-        $("[data-test-id=date] [placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, when(true));
+        $("[data-test-id='success-notification']>.notification__content").shouldBe(visible).shouldHave(text("Встреча успешно запланирована на " + when(0)));
+        $("[data-test-id=date] [placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, when(2));
         $(".grid-col .button__text").click();
         $("[data-test-id='replan-notification'] .button__text").click();
-        $("[data-test-id='success-notification']>.notification__content").shouldHave(text("Встреча успешно запланирована на " + when(true))).shouldBe(visible);
+        $("[data-test-id='success-notification']>.notification__content").shouldBe(visible).shouldHave(text("Встреча успешно запланирована на " + when(2)));
     }
 }
--- src/test/java/ru/netology/util/DataHelper.java
+++ src/test/java/ru/netology/util/DataHelper.java
@@ -5,44 +5,28 @@ import com.github.javafaker.Faker;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Random;
 
 public class DataHelper {
     private DataHelper() {
     }
 
     private static final Faker faker = new Faker(new Locale("ru"));
-    private static final String city = faker.address().cityName().replace("Новокузнецк", "Курск").replace("Сочи", "Тула");
-    private static final String anotherCity = faker.address().cityName().replace("Новокузнецк", "Курск").replace("Сочи", "Тула");
-    private static final String lattermostCity = faker.address().cityName().replace("Новокузнецк", "Курск").replace("Сочи", "Тула");
-    private static final String surname = faker.name().lastName().replace("ё", "е");
-    private static final String name = faker.name().firstName().replace("ё", "е");
-    private static final String fullName = surname + " " + name;
-    private static final String phone = faker.phoneNumber().phoneNumber().replace("+", "").replace("(", "").replace(")", "").replace("-", "");
 
-    public static String when(boolean reschedule) {
-        long daysToAdd = 7;
-        if (reschedule)
-            return LocalDate.now().plusDays(daysToAdd + 2).format(DateTimeFormatter.ofPattern("dd.MM.uuuu"));
-        else return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern("dd.MM.uuuu"));
+    public static String getCityInternally() {
+        String[] city = {"Майкоп", "Горно-Алтайск", "Уфа"};
+        return city[new Random().nextInt(3)];
     }
 
-    public static String getCity() {
-        return city;
-    }
-
-    public static String getAnotherCity() {
-        return anotherCity;
-    }
-
-    public static String getLattermostCity() {
-        return lattermostCity;
+    public static String when(int reschedule) {
+        return LocalDate.now().plusDays(7 + reschedule).format(DateTimeFormatter.ofPattern("dd.MM.uuuu"));
     }
 
     public static String getFullName() {
-        return fullName;
+        return faker.name().lastName().replace("ё", "е") + " " + faker.name().firstName().replace("ё", "е");
     }
 
     public static String getPhone() {
-        return phone;
+        return faker.phoneNumber().phoneNumber().replace("+", "").replace("(", "").replace(")", "").replace("-", "");
     }
 }
